4d81a554c374fe5fad9b2b79ef3717a72ca13f5a
段永强 <duany001@cmft.com>
2019-09-29 11:35:08 +0800

    feat:add url

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A userful package to use '.' to visit value of dict",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/Yyonging/dict_wrapper",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
